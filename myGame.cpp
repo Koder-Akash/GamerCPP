a8d66506651ce57beae5f0f275a2d4fa583d2a56
koderakash <kakash28003@gmail.com>
2024-01-06 12:15:23 +0530

    player style changed

--- myGame.cpp
+++ myGame.cpp
@@ -8,28 +8,28 @@ using namespace std;
 int main()
 {
     int length = 130;
-    
+
     int rand();
-    
+
     cout<<"Enter track length: "<<length<<endl;
-    
+
     //cin>>length;
 
     while(1)
     {
         int temp = rand()%length; // 0 to l-1(l)
-        
+
         cout<<"START";
-        
+
         for(int i=0; i < length; i++)
         {
-        
+
             if(i==temp)
-                cout<<"|";
+                cout<<"|||";
 
             else
                 cout<<".";
-                        
+
         }
 
         cout<<"END"<<endl;
@@ -37,11 +37,11 @@ int main()
         Sleep(1);
 
         if(temp == length-1)
-        {   
+        {
             break;
         }
 
     }
-        
+
     return 0;
 }
